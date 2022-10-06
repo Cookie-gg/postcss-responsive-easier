5e6e039671bbfefd23cb5176df5a0131eef339ec
cookie_gg <cookie.gg.dev@gmail.com>
2022-10-07 03:15:59 +0900

    fix regexp

--- src/libs/regexp/index.ts
+++ src/libs/regexp/index.ts
@@ -1,7 +1,7 @@
 export const REGEXP = {
   CUSTOM_PROPERTY: /^\-{2}/,
   // SPLIT: (syntax: string, flags?: string) => new RegExp(`\\s*\\${syntax}\\s*`, flags),
-  SPLIT: /\s*\\|\s*/,
+  SPLIT: /\s*\|\s*/,
   FN: /^(v?calc|v?rgb[a]?|var)\((.*?)\)$/,
   FN_NAME: /(v?calc|v?rgb[a]?|var)/,
   WRAP: /(（|）)/g,
